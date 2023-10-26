9c0c2be16148314a3dc5e03f6a4ed30278ead729
Siddhant <siddhantrsen73938@gmail.com>
2023-10-26 16:10:43 +0530

    Size table and background

--- src/components/infoBlock.jsx
+++ src/components/infoBlock.jsx
@@ -11,16 +11,52 @@ export default function InfoBlock() {
   const { setModalOpen } = useContext(AppContext);
   return (
     <div className={classes.infoBlock}>
-      <Text size="xs" fs="italic" c="dimmed">
+      <Text size="md" fs="normal" c="dark.4">
         TAX INCLUDED. SHIPPING CALCULATED AT CHECKOUT.
       </Text>
-      <Text size="lg" fw={500}>
+      <Text size="45px" fw={600} c='dark.1'>
         Technosearch Tshirt
       </Text>
-      <Text>INR {price}</Text>
-
-      <Box mt="md">
-        <Button onClick={()=>{setModalOpen(true)}}>Proceed to buy</Button>
+      <Text size='20px' fw={400} c='dark.2'>INR {price}.00 /-</Text>
+      <hr className={classes.hr} />
+      <div className={classes.tablediv}>
+        <h1 className={classes.tabletext}>T-Shirt Size Chart</h1>
+        <table className={classes.table}>
+          <thead className={classes.tr}>
+            <tr className={classes.tr}>
+              <th className={classes.th}>Size  (inches)</th>
+              <th className={classes.th}>Chest (inches)</th>
+              <th className={classes.th}>Length (inches)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className={classes.tr}>
+              <td className={classes.td}>Small</td>
+              <td className={classes.td}>44</td>
+              <td className={classes.td}>28</td>
+            </tr>
+            <tr className={classes.tr}>
+              <td className={classes.td}>Medium</td>
+              <td className={classes.td}>46</td>
+              <td className={classes.td}>29</td>
+            </tr>
+            <tr className={classes.tr}>
+              <td className={classes.td}>Large</td>
+              <td className={classes.td}>48</td>
+              <td className={classes.td}>30</td>
+            </tr>
+            <tr className={classes.tr}>
+              <td className={classes.td}>X-Large</td>
+              <td className={classes.td}>50</td>
+              <td className={classes.td}>31</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <Box mt="md" mb='xl'>
+        <Button bg='dark.4' onClick={() => { setModalOpen(true) }}>
+          <p className={classes.buttonpara}>Proceed to pay</p>
+        </Button>
       </Box>
     </div>
   );
